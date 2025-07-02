1e25072fa103397129ab546f21f2d88e1a1e0528
ivan chernov <96547961+codeWeb777@users.noreply.github.com>
2025-07-02 14:50:35 +0300

    Добавление страницы Филантропия

---
+++ src/app/philantropy/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import BaseHeader from "@/components/BaseHeader";
+import Footer from "@/modules/Footer";
+import Philanthropy from "@/components/Philanthropy";
+
+const PhilanthropyPage = () => {
+  return (
+    <>
+      <BaseHeader />
+      <Philanthropy />
+      <Footer />
+    </>
+  );
+};
+
+export default PhilanthropyPage;
