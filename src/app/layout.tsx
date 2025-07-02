24dfd6fe6fe27b99731f2ff82c2e5e6f313f1005
ivan chernov <96547961+codeWeb777@users.noreply.github.com>
2025-07-02 14:52:38 +0300

    Рефакторинг общего layout и глобальных стилей

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,13 @@
+import type { Metadata } from "next";
 import "./globals.css";
+import React from "react";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const metadata: Metadata = {
+  title: "Lawyer website",
+  description: "Lawyer the best site",
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +16,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className="">
+        {children}
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
+        />
+      </body>
     </html>
   );
 }
